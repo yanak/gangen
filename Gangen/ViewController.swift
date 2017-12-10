48bc6af6543a6310284314396ac139e397d1bcb4
Yasunori Tanaka <ysnr.tanaka@gmail.com>
2017-12-10 12:59:09 +0900

    Initial Commit

---
+++ Gangen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gangen
+//
+//  Created by Yasunori Tanaka on 2017/12/10.
+//  Copyright © 2017 Yasunori Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
